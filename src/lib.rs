e0b0dab71cb743df3b6495b200bb75a5ea5bb703
leo60228 <iakornfeld@gmail.com>
2020-08-07 16:50:39 -0400

    Add Cargo metadata

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! API client for [Crom](https://crom.avn.sh/), a scraper for the [SCP
+//! Wiki](http://www.scpwiki.com/) and related projects.
+
+#![forbid(unsafe_code)]
+#![warn(missing_docs)]
+
 mod graphql;
 
 pub use graphql::search_pages::SearchPagesSearchPages as Page;
@@ -15,14 +21,19 @@ use http_client::native::NativeClient;
 #[cfg(feature = "h1-client")]
 use http_client::h1::H1Client;
 
+/// Error type.
 #[derive(Error, Debug)]
 pub enum Error {
+    /// HTTP error.
     #[error("http error: {0}")]
     Http(surf::Error),
+    /// GraphQL error.
     #[error("graphql error: {}", .0.iter().map(ToString::to_string).collect::<Vec<_>>().join(", "))]
     GraphQL(Vec<graphql_client::Error>),
+    /// JSON error.
     #[error("json error: {0}")]
     Json(#[from] serde_json::Error),
+    /// IO error.
     #[error("io error: {0}")]
     Io(#[from] std::io::Error),
 }
@@ -33,6 +44,7 @@ impl From<surf::Error> for Error {
     }
 }
 
+/// Result type.
 pub type Result<T> = StdResult<T, Error>;
 
 fn res_from_resp<T>(r: Response<T>) -> Result<T> {
@@ -49,12 +61,14 @@ fn res_from_resp<T>(r: Response<T>) -> Result<T> {
     }
 }
 
+/// API client.
 pub struct Client<T: HttpClient> {
     client: surf::Client<T>,
 }
 
 #[cfg(feature = "h1-client")]
 impl Client<H1Client> {
+    /// Create a new client with default settings.
     pub fn new() -> Self {
         Self::with_client(H1Client::new())
     }
@@ -62,17 +76,20 @@ impl Client<H1Client> {
 
 #[cfg(feature = "native-client")]
 impl Client<NativeClient> {
+    /// Create a new client with default settings.
     pub fn new() -> Self {
         Self::with_client(NativeClient::new())
     }
 }
 
 impl<C: HttpClient> Client<C> {
+    /// Create a customized new client.
     pub fn with_client(client: C) -> Self {
         let client = surf::Client::with_client(client);
         Self { client }
     }
 
+    /// Search pages.
     pub async fn search(&self, query: &str) -> Result<Vec<Page>> {
         use graphql::{search_pages::*, SearchPages};
 
