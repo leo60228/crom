972b157674001fd84c03d341ab640b862e776e9a
leo60228 <iakornfeld@gmail.com>
2020-08-07 16:44:20 -0400

    error handling rewrite/fix

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,50 @@ pub use graphql::search_pages::SearchPagesSearchPages as Page;
 pub use graphql::search_pages::SearchPagesSearchPagesWikidotInfo as WikidotInfo;
 pub use graphql::search_pages::SearchPagesSearchPagesWikidotInfoCreatedBy as CreatedBy;
 
-use graphql_client::GraphQLQuery;
-
+use graphql_client::{GraphQLQuery, Response};
 use http_client::HttpClient;
+use std::result::Result as StdResult;
+use thiserror::Error;
 
 #[cfg(feature = "native-client")]
 use http_client::native::NativeClient;
 
 #[cfg(feature = "h1-client")]
 use http_client::h1::H1Client;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("http error: {0}")]
+    Http(surf::Error),
+    #[error("graphql error: {}", .0.iter().map(ToString::to_string).collect::<Vec<_>>().join(", "))]
+    GraphQL(Vec<graphql_client::Error>),
+    #[error("json error: {0}")]
+    Json(#[from] serde_json::Error),
+    #[error("io error: {0}")]
+    Io(#[from] std::io::Error),
+}
+
+impl From<surf::Error> for Error {
+    fn from(e: surf::Error) -> Self {
+        Self::Http(e)
+    }
+}
+
+pub type Result<T> = StdResult<T, Error>;
+
+fn res_from_resp<T>(r: Response<T>) -> Result<T> {
+    match r {
+        Response {
+            errors: Some(errors),
+            ..
+        } => Err(Error::GraphQL(errors)),
+        Response {
+            data: Some(data),
+            errors: None,
+        } => Ok(data),
+        _ => Err(Error::GraphQL(vec![])),
+    }
+}
 
 pub struct Client<T: HttpClient> {
     client: surf::Client<T>,
@@ -38,14 +73,14 @@ impl<C: HttpClient> Client<C> {
         Self { client }
     }
 
-    pub async fn search(&self, query: &str) -> surf::Result<Vec<Page>> {
+    pub async fn search(&self, query: &str) -> Result<Vec<Page>> {
         use graphql::{search_pages::*, SearchPages};
 
         let query = query.to_string();
         let variables = Variables { query };
         let body = SearchPages::build_query(variables);
 
-        let resp: ResponseData = self
+        let resp: Response<ResponseData> = self
             .client
             .post("https://api.crom.avn.sh/")
             .body_json(&body)?
@@ -53,6 +88,6 @@ impl<C: HttpClient> Client<C> {
             .body_json()
             .await?;
 
-        Ok(resp.search_pages)
+        Ok(res_from_resp(resp)?.search_pages)
     }
 }
